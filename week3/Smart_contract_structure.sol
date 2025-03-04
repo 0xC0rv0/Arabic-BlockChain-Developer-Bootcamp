4870b9f259145c927590803e4123410fac789444
0xc0rv0 <crow7@protonmail.com>
2025-03-04 21:49:00 +0200

    lec 5

--- week3/Smart_contract_structure.sol
+++ week3/Smart_contract_structure.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.26; // Allows Solidity 0.8.0 and all later version within 0.8.x
 
 contract MyContract {
     string public messsage = "Hello World";
